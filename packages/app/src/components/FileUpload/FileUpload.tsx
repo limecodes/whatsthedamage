94929b311966c560d45018d1a1b196636492da17
limecodes <yan.shcherbakov+limecodes@gmail.com>
2023-05-25 09:44:47 +0300

    feat(app): Started csv upload step.

---
+++ packages/app/src/components/FileUpload/FileUpload.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export function FileUpload() {
+  return (
+  	<div>
+  		<input
+  		  type="file"
+  		  onChange={() => { console.log('upload attempt') }}
+  		/>
+  		<button>Upload</button>
+  	</div>
+  )
+}
