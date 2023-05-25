c6719b34749adc9b83ef3c563d43b11ecc5c18a0
limecodes <yan.shcherbakov+limecodes@gmail.com>
2023-05-25 09:45:50 +0300

    feat(app): Started csv upload step.

--- packages/app/src/components/App/App.tsx
+++ packages/app/src/components/App/App.tsx
@@ -1,5 +1,22 @@
 import React from 'react'
+import Container from '@mui/material/Container'
+import Card from '@mui/material/Card'
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent'
+import { FileUpload } from '../FileUpload'
 
 export function App() {
-  return <div className="App">Start</div>
+  return (
+    <Container>
+      <Card>
+        <CardHeader title="Step 1" />
+        <CardContent>
+          <p>Upload your statement in csv format.</p>
+          <p>Retrieve a CSV statement for the period that you want to analyse.</p>
+          <p>Normally it's from your last pay up to the latest pay.</p>
+          <FileUpload />
+        </CardContent>
+      </Card>
+    </Container>
+  )
 }
